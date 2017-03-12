1489b7f8f4730a0e096c5e2b91c6b88d8c7d2584
cuieney <Cui131425>
2017-03-12 15:45:34 +0800

    fix issues
    fix issues

--- app/src/main/java/org/cuieney/videolife/common/api/UrlManager.java
+++ app/src/main/java/org/cuieney/videolife/common/api/UrlManager.java
@@ -1,10 +1,7 @@
 package org.cuieney.videolife.common.api;
 
 public class UrlManager {
-
-
     public static final String KAIYAN_HOST = "https://baobab.kaiyanapp.com/api/v4/" ;
     public static final String WANGYI_HOST = "http://www.wawa.fm:9090/wawa/api.php/magazine/";
-
 
 }
